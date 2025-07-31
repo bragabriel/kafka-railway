14ea8cc715e10a59d8696bcc173247ba4628ecba
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2025-07-30 21:29:18 -0300

    chore(kafka-ports): align Kafka bootstrap servers with Docker Compose external port

--- market/src/main/java/io/github/bragabriel/kafka_railway/config/KafkaConsumerConfig.java
+++ market/src/main/java/io/github/bragabriel/kafka_railway/config/KafkaConsumerConfig.java
@@ -18,22 +18,10 @@ public class KafkaConsumerConfig {
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
 
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:19092");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "default-group");
 
         return new DefaultKafkaConsumerFactory<>(props);
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-
-        factory.setConsumerFactory(consumerFactory());
-
-        return factory;
     }
 }
